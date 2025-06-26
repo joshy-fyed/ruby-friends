5a1ddd1d28058564adedc858bb9c315fb9f27c9e
visayan <195053283+joshy-fyed@users.noreply.github.com>
2025-06-26 18:12:16 +0900

    add bootstrap; revise design

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # generates index, show, new, create, update, destroy
   resources :friends
   get "home/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -7,7 +8,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   get "home/about"
-  root "home#index"
+  root to: "home#index"
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
